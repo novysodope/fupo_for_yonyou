db34d26be4f7b9927f06e936a2f6e35eebc3db94
novy <45167857+novysodope@users.noreply.github.com>
2024-01-11 17:53:19 +0800

    Create Parser.go

---
+++ Parser.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"golang.org/x/net/proxy"
+	"net/http"
+	"time"
+)
+
+func targetParse(address string, proxyAddr string, Red string, Green string, Yellow string, Reset string, Cyan string) {
+	if proxyAddr != "" {
+		// 使用 socks5 代理创建 Dialer
+		dialer, err := proxy.SOCKS5("tcp", proxyAddr, nil, proxy.Direct)
+		if err != nil {
+			fmt.Println("创建 SOCKS5 代理失败:", err)
+			return
+		}
+		tr := &http.Transport{
+			//socks5代理
+			Dial: dialer.Dial,
+		}
+		client := &http.Client{
+			Transport: tr,
+			Timeout:   10 * time.Second,
+		}
+		client.Transport = tr
+	}
+	tr := &http.Transport{
+		//忽略https证书错误
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{
+		Transport: tr,
+	}
+
+	moduleConf(address, client, Red, Green, Yellow, Reset, Cyan)
+}
