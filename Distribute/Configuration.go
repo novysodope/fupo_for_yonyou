455b1385b0372d332ddfdca194db30a0856495d3
novy <45167857+novysodope@users.noreply.github.com>
2024-01-12 18:57:04 +0800

    Create Configuration.go

---
+++ Distribute/Configuration.go
@@ -0,0 +1,13 @@
+package Distribute
+
+import (
+	"FoPoForYonyou2.0/Scan"
+	"net/http"
+	"time"
+)
+
+func ModuleConf(address string, client *http.Client, Red string, Green string, Yellow string, Reset string, Cyan string) {
+	currentTime := time.Now().Format("15:04:05")
+	Scan.SyncScan(address, Red, Green, Yellow, Reset, Cyan, currentTime)
+//支持自定义漏洞URL文件
+}
