54d093bfc0d6b48c13a1e268d16b4b48d53fb93d
novy <45167857+novysodope@users.noreply.github.com>
2024-01-12 17:58:14 +0800

    Update Configuration.go

--- Configuration.go
+++ Configuration.go
@@ -3,9 +3,11 @@ package main
 import (
 	"net/http"
 	"time"
+	"fupo_for_yonyou/Scan"
 )
 
 func moduleConf(address string, client *http.Client, Red string, Green string, Yellow string, Reset string, Cyan string) {
 	currentTime := time.Now().Format("15:04:05")
-	syncScan(address, currentTime, Green, Yellow, Reset, Cyan, Red)
+	syncScan(address, Red, Green, Yellow, Reset, Cyan, currentTime)
+	//增加自定义yaml支持
 }
